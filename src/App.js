02696a519a4ea6482ddc8129e397cbdb6a78f3b7
tamm19512 <tamm19512@hotmail.com>
2020-02-28 16:48:41 +0700

    ADD dropdown

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Home from './Home.js';
-import Bisection from './Bisection.js';
-import FalsePos from './FalsePos.js';
-import test from './test.js';
+import 'antd/dist/antd.css';
+import { Menu, Dropdown, Button } from 'antd';
+
+import Home from './Home'
+import Bisection from './Bisection'
+import FalsePos from './FalsePos'
+import test from './test'
 
 import {
   Route,
   NavLink,
   Switch
 } from "react-router-dom";
+
+
+const menu1 = (
+
+  
+
+  <Menu>
+
+    <div className="nav">
+
+        <Menu.Item>
+            <NavLink exact to ="/" activeClassName="active">Home </NavLink>
+        </Menu.Item>
+        
+        <Menu.Item>
+            <NavLink to ="/Bisection" activeClassName="active">Bisection </NavLink>
+        </Menu.Item>
+
+        <Menu.Item>
+            <NavLink to ="/FalsePos" activeClassName="active">FalsePos</NavLink>  
+        </Menu.Item>
+
+        <Menu.Item>
+            <NavLink to ="/test" activeClassName="active">test</NavLink>
+        </Menu.Item>
+        
+    </div>
+
+  </Menu>
+
+
+);
+
+
 
 class App extends Component {
   
@@ -20,13 +57,33 @@ class App extends Component {
 
     <div className="App">
 
+    <div className="nav">
 
-      <div className="nav">
+        <Dropdown overlay={menu1} placement="bottomLeft">
 
-         <NavLink exact to ="/" activeClassName="active">Home </NavLink>
-         <NavLink to ="/Bisection" activeClassName="active">Bisection </NavLink>
-         <NavLink to ="/FalsePos" activeClassName="active">FalsePos</NavLink>
-         <NavLink to ="/test" activeClassName="active">test</NavLink>
+          <button>Root of Equations</button>
+
+        </Dropdown>
+
+      <h>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h>
+      
+
+        <Dropdown overlay={menu1} placement="bottomLeft">
+
+          <button>Linear</button>
+
+        </Dropdown>
+
+      <h>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h>
+      
+
+        <Dropdown overlay={menu1} placement="bottomLeft">
+
+          <button>Iterative Method</button>
+
+        </Dropdown>
+         
+         
 
       </div>
            
@@ -42,8 +99,9 @@ class App extends Component {
     </div>
 
     );
+    
   }
+  
 
 }
-
 export default App;
--- src/Bisection.js
+++ src/Bisection.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react'
 import logo from './logo.svg';
 
+import { Card, Input, Button, Table, Col, Row } from 'antd';
+import 'antd/dist/antd.css';
+import { range, compile } from 'mathjs'
 
 
-const Bisection = () =>{
 
-    return(
-        <div className = "App">
-         <section>
 
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1>How to create animated page transitions in React?</h1>
+class Bisection extends Component{
 
-            </section>
-        </div>
-    )
+    render() {
 
+        return(
+
+                <div className = "App">
+                <section>
+
+                        <img src={logo} className="App-logo" alt="logo" />
+                        <h1>How to create animated page transitions in React?</h1>
+
+                    </section>
+                </div>
+
+        );
+    
+    }
     
 
 }
-
 export default Bisection;
